22894eba0385d094db7e49a444f15286016c3c42
andré Machado <andre@hiper.com.br>
2023-06-22 16:45:34 -0300

    CM - Realizado ajuste de conexao e realizado a mgiration para criação do banco

---
+++ Microservices.Produtos.CrossCutting/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using Microservices.Produtos.Domain.Interfaces;
+using Microservices.Produtos.Infrastructure.Context;
+using Microservices.Produtos.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microservices.Produtos.CrossCutting
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+           IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly(typeof(AppDbContext)
+                            .Assembly.FullName)));
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            return services;
+        }
+    }
+}
---
+++ Microservices.Produtos.Infrastructure/Repositories/ProdutoRepository.cs
@@ -0,0 +1,59 @@
+﻿using Microservices.Produtos.Domain.Entities;
+using Microservices.Produtos.Domain.Interfaces;
+using Microservices.Produtos.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microservices.Produtos.Infrastructure.Repositories
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+        protected readonly AppDbContext db;
+        public ProdutoRepository(AppDbContext context)
+        {
+            db = context;
+        }
+        public void Add(Produto produto)
+        {
+            db.Produtos.Add(produto);
+            db.SaveChanges();
+        }
+
+        public async Task<IEnumerable<Produto>> GetAll()
+        {
+            return await db.Produtos.ToListAsync();
+        }
+
+        public async Task<Produto> GetByCodigoBarras(string codigoBarras)
+        {
+            return await db.Produtos.AsNoTracking().FirstOrDefaultAsync(x => x.CodigoBarras == codigoBarras);
+        }
+
+        public async Task<Produto> GetById(int id)
+        {
+            return await db.Produtos.FindAsync(id);
+        }
+
+        public  async Task<Produto> GetByNome(string nomeProduto)
+        {
+            return await db.Produtos.AsNoTracking().FirstOrDefaultAsync(x => x.NomeProduto == nomeProduto);
+        }
+
+        public void Remove(Produto produto)
+        {
+            db.Produtos.Remove(produto);
+            db.SaveChanges();
+        }
+
+        public void Update(Produto produto)
+        {
+            db.Produtos.Update(produto);
+            db.SaveChanges();
+        }
+    }
+}
+
