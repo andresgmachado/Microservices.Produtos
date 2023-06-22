eb4ee3d688a23d1593f561ede36d743aa6d462dd
andré Machado <andre@hiper.com.br>
2023-06-22 17:22:44 -0300

    CM - Implementando CQRS e controller

---
+++ Microservices.Produtos.API/Controllers/ProdutoController.cs
@@ -0,0 +1,62 @@
+﻿using MediatR;
+using Microservices.Produtos.Application.Services.Commands;
+using Microservices.Produtos.Application.Services.Queries;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Microservices.Produtos.API.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+        private IMediator _mediator;
+        public ProdutoController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateProdutoCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _mediator.Send(new GetAllProdutosQuery());
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var result = await _mediator.Send(new GetProdutoByIdQuery { Id = id });
+            if (result == null)
+                return NotFound();
+            else
+                return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _mediator.Send(new DeleteProdutoByIdCommand { Id = id });
+            return Ok(result);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, UpdateProdutoCommand command)
+        {
+            if (id != command.ProdutoId)
+            {
+                return BadRequest();
+            }
+
+            var result = await _mediator.Send(command);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ Microservices.Produtos.Application/Services/Commands/DeleteProdutoByIdCommand.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microservices.Produtos.Domain.Interfaces;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microservices.Produtos.Application.Services.Commands
+{
+    public class DeleteProdutoByIdCommand : IRequest<int>
+    {
+        public int Id { get; set; }
+
+        public class DeleteProductByIdCommandHandler : IRequestHandler<DeleteProdutoByIdCommand, int>
+        {
+            private readonly IProdutoRepository _context;
+            private readonly IMediator _mediator;
+            public DeleteProductByIdCommandHandler(IProdutoRepository context, IMediator mediator)
+            {
+                _context = context;
+                _mediator = mediator;
+            }
+
+            public async Task<int> Handle(DeleteProdutoByIdCommand command, CancellationToken cancellationToken)
+            {
+                var produto = await _context.GetById(command.Id);
+                if (produto == null) return default;
+                _context.Remove(produto);
+                return produto.ProdutoId;
+            }
+        }
+    }
+}
