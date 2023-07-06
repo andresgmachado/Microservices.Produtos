f4c1b1e3c70e6913e34f2775d62cfdea5423b3d7
andré Machado <andre@hiper.com.br>
2023-07-06 16:59:34 -0300

    CM -  Ajuste nuget

---
+++ Microservices.Produtos.Application/Services/Events/ProdutoCreateEvent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microservices.Produtos.Application.Services.Events
+{
+    public class ProdutoCreateEvent : IEvent
+    {
+        public ProdutoCreateEvent(Guid id, string nomeProduto)
+        {
+            Id = id;
+            NomeProduto = nomeProduto;
+        }
+
+        public Guid Id { get; }
+        public string NomeProduto { get; }
+    }
+}
+
